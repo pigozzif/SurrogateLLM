0f5e1561957a5bdd60f6372cd949a56100e448d2
pigozzif <pigozzife@gmail.com>
2023-12-07 21:28:17 +0100

    fix plots

--- plot_results.py
+++ plot_results.py
@@ -13,10 +13,12 @@ def read_files(path):
         d["approach"] = file.split(".")[0]
         d["problem"] = file.split(".")[1]
         d["seed"] = int(file.split(".")[2])
+        d = d[d["iter_idx"] != "iter_idx"]
         d["iter_idx"] = d.apply(lambda row: int(row["iter_idx"]), axis=1)
-        d = d[~d.str.contains("iter_idx", na=False)]
+        for var in ["iter_best_fitness", "iter_eval_time", "iter_model_time"]:
+            d[var] = d[var].astype(np.float64)
         d["iter_total_time"] = d["iter_eval_time"] + d["iter_model_time"]
-        d["iter_total_time"] = d["iter_total_time"].cumsum()
+        # d["iter_total_time"] = d["iter_total_time"].cumsum()
         if data is None:
             data = pd.DataFrame(columns=d.columns)
         data = pd.concat([data, d])
@@ -24,14 +26,14 @@ def read_files(path):
 
 
 def plot_vars(data, vs):
-    fig, axes = plt.subplots(figsize=(20, 10), nrows=len(vs), ncols=len(data["problem"].unique()))
+    fig, axes = plt.subplots(figsize=(20, 20), nrows=len(vs), ncols=len(data["problem"].unique()))
     for row, var in enumerate(vs):
         for col, (problem, traj) in enumerate(data.groupby(["problem"])):
             for approach, inner_traj in traj.groupby(["approach"]):
                 median = inner_traj.groupby(inner_traj.iter_idx)[var].median()
                 axes[row][col].plot(median, label=approach)
                 err = inner_traj.groupby(inner_traj.iter_idx)[var].std()
-                axes[row][col].fill_between(np.arange(len(median)), median - err, median + err)
+                axes[row][col].fill_between(np.arange(len(median)), median - err, median + err, alpha=0.25)
             axes[row][col].set_title(problem, fontsize=15)
             axes[row][col].set_xlabel("iters", fontsize=15)
             axes[row][col].legend()
@@ -40,6 +42,6 @@ def plot_vars(data, vs):
 
 if __name__ == "__main__":
     data = read_files("results")
-    plot_vars(data, ["iter_best_fitness", "iter_total_time"])
+    plot_vars(data, ["iter_best_fitness", "iter_eval_time", "iter_model_time", "iter_total_time"])
     plt.savefig("results.png")
     plt.clf()
